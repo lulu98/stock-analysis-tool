9bf8b2b3c83fe6ec16b55d48757b48e381230fea
lulu98 <lukas-graber@protonmail.com>
2022-05-02 18:51:57 +0200

    Refactor stage02.py

--- src/stage02.py
+++ src/stage02.py
@@ -20,42 +20,104 @@ from scripts.calculations import *
 
 def setEnvironmentVariables(isin):
     """
-    Sets environment variables required for stage 1 of the stock analysis 
-    pipeline.
+    Sets environment variables required by stage 2.
+    """
+    rootDir = os.path.abspath(os.getcwd())
+
+    templateDir = os.path.join(rootDir, "template")
+    dataDir = os.path.join(rootDir, "data", isin)
+    buildDir = os.path.join(rootDir, "stocks", isin)
+
+    jsonFile = os.path.join(buildDir, "data", "data.json")
+
+    os.environ['ROOT_DIR'] = rootDir
+    os.environ['DATA_DIR'] = dataDir 
+    os.environ['TEMPLATE_DIR'] = templateDir 
+    os.environ['BUILD_DIR'] = buildDir
+    os.environ['JSON_FILE'] = jsonFile
+
+def isStage01Done():
+    dataDir = os.getenv('DATA_DIR')
+    return os.path.exists(dataDir)
+
+def isBuildDirectoryExists():
+    """Returns if build directory exists."""
+    buildDir = os.getenv('BUILD_DIR')
+    return os.path.exists(buildDir)
+
+def isFilesTheSame(srcFile, dstFile):
+    """Returns if srcFile and dstFile are the same."""
+    return filecmp.cmp(srcFile, dstFile)
+
+def setupBuildDirectory(srcFile, dstFile):
+    """Sets up build directory."""
+    templateDir = os.getenv('TEMPLATE_DIR')
+    buildDir = os.getenv('BUILD_DIR')
+
+    print("Initial directory setup started...")
+    os.makedirs(buildDir)
+    shutil.copytree(templateDir, buildDir, dirs_exist_ok=True)
+    shutil.copyfile(srcFile, dstFile)
+    print("Initial directory setup done.")
+
+def updateDataDirectory(srcFile, dstFile):
+    """Updates data directory."""
+    templateDir = os.getenv('TEMPLATE_DIR')
+    buildDir = os.getenv('BUILD_DIR')
+
+    templateDataDir = os.path.join(templateDir, "data")
+    buildDataDir = os.path.join(buildDir, "data")
+
+    print("Changes in data detected.")
+    print("Rebuild data directory started...")
+    shutil.copytree(templateDataDir, buildDataDir, dirs_exist_ok=True)
+    shutil.copyfile(srcFile, dstFile)
+    print("Rebuild data directory done.")
+
+def checkAndInitializeBuildDir(srcFile, dstFile):
+    """Set up build directory once initially."""
+    if not isBuildDirectoryExists():
+        setupBuildDirectory(srcFile, dstFile)
+
+def checkAndUpdateDataDir(srcFile, dstFile):
+    """If the financial data changed, update the data directory of the build."""
+    if not isFilesTheSame(srcFile, dstFile):
+        updateDataDirectory(srcFile, dstFile)
+
+def prepareBuildDir(isin):
+    """Prepare build directory for stage 2 execution."""
+    jsonSrc = os.path.join(os.getenv('DATA_DIR'), "data.json")
+    jsonDst = os.getenv('JSON_FILE')
+
+    if not isStage01Done():
+        sys.exit("Data for company {} not available. Execute stage 1 first.".format(isin))
+
+    checkAndInitializeBuildDir(jsonSrc, jsonDst)
+
+    checkAndUpdateDataDir(jsonSrc, jsonDst)
+
+def prepareStage(isin):
+    """
+    Prepares stage 2 of the stock analysis pipeline. 
 
     Parameters:
         isin (str): ISIN of the stock to be analysed.
     """
-    rootDir = os.path.abspath(os.getcwd())
-    templateDir = os.path.join(rootDir, "template")
-    dataDir = os.path.join(rootDir, "data", isin)
-    companyDir = os.path.join(rootDir, "stocks", isin)
-    companyDataDir = os.path.join(companyDir, "data")
-    jsonSrc = os.path.join(dataDir, "data.json")
-    jsonDst = os.path.join(companyDataDir, "data.json")
+    setEnvironmentVariables(isin)
+    prepareBuildDir(isin)
 
-    if not os.path.exists(dataDir):
-        sys.exit("Data for company {} not available. Execute stage 1 first.".format(isin))
+def formatStringToSedInput(text):
+    """Format string input into sed compliant input string."""
+    output = text.replace("\\", "\\\\")
+    return output
 
-    # setup the company folder once initially
-    if not os.path.exists(companyDir):
-        print("Initial setup started...")
-        os.makedirs(companyDir)
-        shutil.copytree(templateDir, companyDir, dirs_exist_ok=True)
-        shutil.copyfile(jsonSrc, jsonDst)
-        print("Initial setup done.")
-
-    # if the data changed, pull the changes into the data folder
-    if not filecmp.cmp(jsonSrc, jsonDst):
-        print("Changes in data detected.")
-        print("Rebuild data directory started...")
-        shutil.copytree(os.path.join(templateDir, "data"), companyDataDir, dirs_exist_ok=True)
-        shutil.copyfile(jsonSrc, jsonDst)
-        print("Rebuild data directory done.")
-
-    os.environ['ROOT_DIR'] = rootDir
-    os.environ['DATA_DIR'] = companyDataDir
-    os.environ['JSON_FILE'] = jsonDst
+def formatStringToSedOutput(text):
+    """Format string input into sed compliant output string."""
+    output = text.replace("&", "\&")
+    output = output.replace("/", "\/")
+    output = output.replace("\n", "\\n")
+    output = output.replace("'", "'\"'\"'")
+    return output
 
 def findPattern(regex, fileName):
     '''
@@ -83,10 +145,6 @@ def replacePattern(regex, output, fileName):
         output (str): Pattern to be replaced.
         fileName (str): Name of the file.
     '''
-    output = str(output).replace("&", "\&") # sed pattern & should be replaced
-    output = output.replace("/", "\/")      # sed pattern / should be replaced
-    output = output.replace("\n", "\\n")    # sed pattern newline should be replaced
-    output = output.replace("'", "'\"'\"'") # sed pattern ' should be replaced
     cmd = "sed 's/{}/{}/g' {}".format(regex, output, fileName)
     output = subprocess.check_output(cmd, shell=True).decode('utf-8')
     with open(fileName, "w") as f:
@@ -106,12 +164,13 @@ def findAndReplacePattern(regex, fileName):
 
     while(len(output) > 0): # while loop to cope with multiple placeholders in one line
         for placeholder in output:
-            pattern = placeholder.replace("\\", "\\\\") # sed pattern must have double backslash
-            replacement = eval(placeholder.replace('__', '')) # the first two underscores must be deleted
+            pattern = formatStringToSedInput(placeholder)
+            replacement = str(eval(placeholder.replace('__', ''))) # the first two underscores must be deleted
+            replacement = formatStringToSedOutput(replacement)
             replacePattern(pattern, replacement, fileName)
         output = findPattern(regex, fileName)
 
-def stage02():
+def executeStage(isin):
     """
     Find and replace patterns with two underscores in the template JSON data 
     files. After replacing all occurrences of patterns in the JSON files, this 
@@ -119,7 +178,7 @@ def stage02():
     As a result, we can access the data from the JSON files like an array access 
     in the Latex files.
     """
-    dataDir = os.getenv('DATA_DIR')
+    dataDir = os.path.join(os.getenv('BUILD_DIR'), "data")
     files = [os.path.join(dataDir, "fund_data.json"),
              os.path.join(dataDir, "calc_data.json")]
 
@@ -150,8 +209,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    setEnvironmentVariables(args.isin)
+    prepareStage(args.isin)
 
     print("Stage 2: started...")
-    stage02()
+    executeStage(args.isin)
     print("Stage 2: done...\n")
