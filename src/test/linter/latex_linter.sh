963d2c08f150e3c82b52a604f1cb237b6ceaea4f
lulu98 <lukas-graber@protonmail.com>
2022-05-07 20:43:34 +0200

    Add Latex linter script

---
+++ src/test/linter/latex_linter.sh
@@ -0,0 +1,29 @@
+CODE_DIR="../../main"
+LATEX_DIR="$CODE_DIR/resources/template"
+
+collect_sources () {
+    latex_files=($LATEX_DIR/chapters/*)
+}
+
+execute_lacheck () {
+    for file in "${latex_files[@]}"; do
+        lacheck "$file"
+    done
+}
+
+execute_chktex () {
+    for file in "${latex_files[@]}"; do
+        chktex -q -n 8 -n 44 "$file"
+    done
+}
+
+process () {
+    echo "Execute lacheck"
+    execute_lacheck
+
+    echo "Execute chktex"
+    execute_chktex
+}
+
+collect_sources
+process
