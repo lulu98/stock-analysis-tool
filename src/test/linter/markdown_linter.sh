d6915a53ce321a5bcdb835a7b4cc39d9e68e7abc
lulu98 <lukas-graber@protonmail.com>
2022-05-08 10:19:03 +0200

    Add Markdown linter script

---
+++ src/test/linter/markdown_linter.sh
@@ -0,0 +1,14 @@
+PROJECT_DIR="../../.."
+
+collect_sources () {
+    markdown_files=($PROJECT_DIR/*.md)
+}
+
+process () {
+    for file in "${markdown_files[@]}"; do
+        mdl -r ~MD024 $file
+    done
+}
+
+collect_sources
+process
